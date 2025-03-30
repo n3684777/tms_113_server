8c2fd5be35e2090f50cead0254fa18192480b60c
n3684777 <38023267+n3684777@users.noreply.github.com>
2025-03-30 23:26:54 +0800

    Update 創角傳送.js

--- scripts/special/創角傳送.js
+++ scripts/special/創角傳送.js
@@ -3,7 +3,9 @@ var 萬能NPC書 = "2450012";
 function start()
 {
     cm.gainItem(輪迴碑石,1);
-    cm.dispose(萬能NPC書,1);
+    cm.gainItem(萬能NPC書,1);    
     cm.warp(104000000);
+    cm.dispose();
+    
 }
   
