cf9f808d74d1cffd178fba8252d0ca444bf386ba
n3684777 <38023267+n3684777@users.noreply.github.com>
2025-03-30 23:16:49 +0800

    item

--- scripts/special/傳送.js
+++
@@ -1,6 +0,0 @@
-function start()
-{
-    cm.warp(104000000);
-    cm.dispose();
-}
-  
---
+++ scripts/special/創角傳送.js
@@ -0,0 +1,9 @@
+var 輪迴碑石 = "2450002";
+var 萬能NPC書 = "2450012";
+function start()
+{
+    cm.gainItem(輪迴碑石,1);
+    cm.dispose(萬能NPC書,1);
+    cm.warp(104000000);
+}
+  
